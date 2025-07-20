379ebffb920042126f0808b4fee91a1e3a13a4a5
zevhoo <zevobh@gmail.com>
2025-07-20 14:10:40 -0400

    Update generate_index.js

--- generate_index.js
+++ generate_index.js
@@ -1,26 +1,20 @@
 const fs = require('fs');
 const path = require('path');
 
-const postDir = './posts';
-const files = fs.readdirSync(postDir).filter(f => f.endsWith('.html'));
+const postsDir = './posts';
+const files = fs.readdirSync(postsDir).filter(f => f.endsWith('.html'));
 
-const listItems = files.map(f =>
-  `<li><a href="posts/${f}">${f}</a></li>`
-).join('\n');
+const links = files.map(f => `<li><a href="posts/${f}">${f}</a></li>`).join('\n');
 
 const html = `
 <!DOCTYPE html>
 <html>
-<head>
-  <title>My Blog</title>
-</head>
+<head><title>My Posts</title></head>
 <body>
-  <h1>Welcome to My Blog</h1>
-  <p>This is a collection of random HTML posts.</p>
-
-  <h2>Posts</h2>
+  <h1>Welcome to My Site</h1>
+  <p>This is an auto-generated list of posts:</p>
   <ul>
-    ${listItems}
+    ${links}
   </ul>
 </body>
 </html>
